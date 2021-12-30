240e086b7392f8097073e2ee9355ad9ba425a0c9
uxchar <c33.j@outlook.com>
2021-12-30 16:50:59 -0500

    fixed email input box, added styling with js

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function checkEmail() {
   var email = document.querySelector(".email-input-field");
+  var email_error_text = document.querySelector(".error-message");
+  var email_error_icon = document.querySelector(".error-icon");
+  var email_input_container = document.querySelector(".email-container");
+  var email_input_field = document.querySelector(".email-input-field");
+
   var emailTest =
     /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
+  // input reset
+
+  email_error_icon.style.visibility = "hidden";
+  email_input_container.style.border = "1px solid #ce9797";
+
+  // email validation
+
   if (!emailTest.test(email.value)) {
-    alert("Email address apears invalid, please provide your email address.");
+    email_error_text.innerHTML = "Please provide a valid email";
+    email_error_text.style.color = " 	#f96262";
+    email_error_text.style.visibility = "visible";
+    email_error_icon.style.visibility = "visible";
+    email_input_container.style.border = "2px solid #f96262";
     email.focus;
     return false;
+  } else {
+    email_error_text.innerHTML = "Thanks for signing up with us!";
+    email_error_text.style.color = "#ce9797";
+    email_input_field.value = "";
+    email_input_field.trim();
+    email_error_icon.style.visibility = "hidden";
+    email_input_container.style.border = "1px solid #ce9797";
   }
 }
